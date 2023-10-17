7b7a7ed1e2e9ac45e8c010da34825e2464bfa372
Andrej Sipka <andrei.sipka@gmail.com>
2023-10-17 20:00:13 +0200

    changes in environemnt setting

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyDpF9r7v-GLz3t2Q0ssN7c-dMF8IM-IDAI",
-        authDomain: "event-map-cfea5.firebaseapp.com",
-        projectId: "event-map-cfea5",
-        storageBucket: "event-map-cfea5.appspot.com",
-        messagingSenderId: "78822569155",
-        appId: "1:78822569155:web:7d857b338c16f71d8c5e76",
-        measurementId: "G-Z2KBQZ2REY"
+        apiKey: "*****************************",
+        authDomain: "*****************************",
+        projectId: "*****************************",
+        storageBucket: "*****************************",
+        messagingSenderId: "*****************************",
+        appId: "*****************************",
+        measurementId: "*****************************"
     }
 };
